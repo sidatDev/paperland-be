96cd4d27017a748cac5b1537fec9cad5f11d7506
Aqib-Seehar <aqib.ali@sidat.net>
2026-05-05 16:54:04 +0500

    feat: add popup image and YouTube link fields to promotions model and implement routing support

---
+++ migration_utf8.sql
@@ -0,0 +1,16 @@
+﻿-- DropForeignKey
+ALTER TABLE "cart_items" DROP CONSTRAINT "cart_items_promotion_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "order_items" DROP CONSTRAINT "order_items_promotion_id_fkey";
+
+-- AlterTable
+ALTER TABLE "cart_items" DROP COLUMN "promotion_id";
+
+-- AlterTable
+ALTER TABLE "order_items" DROP COLUMN "promotion_id";
+
+-- AlterTable
+ALTER TABLE "promotions" ADD COLUMN     "popup_image_desktop" TEXT,
+ADD COLUMN     "popup_image_mobile" TEXT;
+
